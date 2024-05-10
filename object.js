fb4f91760e1e5b51c1cc75b6bdd62919a9ad5796
Muhammad Muntazir Mehdi <MehdiKhosa50>
2024-05-10 16:15:08 +0500

    Lecture01 has been commited

---
+++ object.js
@@ -0,0 +1,13 @@
+console.log("Hello World");  //Printing
+
+// creating object 
+const Profile = {
+    fullName : "Muhammad Muntazir Mehdi",
+    age : 22,
+    hobby : "Travelling",
+    height : "5.6",
+};
+// Printing Object
+console.log(Profile.fullName); 
+console.log(Profile.age);
+console.log(Profile);
