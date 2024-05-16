f41a35f0d737828f32da60f6ea1fc1cc654c6be4
Muhammad Muntazir Mehdi <MehdiKhosa50>
2024-05-16 08:32:23 +0500

    Test.js to run Blockchain.js

---
+++ test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+bitcoin.createNewBlock(100,"ftgyhusijkiohuy","ftgyuhihygtfryg");
+bitcoin.createNewBlock(100,"ftgyhusijkiohuy","ftgyuhihygtfryg");
+bitcoin.createNewBlock(100,"ftgyhusijkiohuy","ftgyuhihygtfryg");
+bitcoin.createNewBlock(100,"ftgyhusijkiohuy","ftgyuhihygtfryg");
+bitcoin.createNewBlock(100,"ftgyhusijkiohuy","ftgyuhihygtfryg");
+console.log(bitcoin);
