ffdd3d7c8b098dbbd629d80678ddfe58c7bcd141
Muhammad Muntazir Mehdi <MehdiKhosa50>
2024-05-16 16:01:12 +0500

    DummyTransactions has been added

--- blockchain.js
+++ blockchain.js
@@ -16,5 +16,17 @@ Blockchain.prototype.createNewBlock = function(nonce,prevHash,hash){
     this.chain.push(newBlock);
     return newBlock;
 }
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length-1];
+}
+Blockchain.prototype.createNewTransactions = function(amount,sender,reciver){
+    var newTransactions={
+        amount: amount,
+        sender: sender,
+        reciver: reciver,
+    };
+    this.pendingTransactions.push(newTransactions);
+    return this.getLastBlock()['index'+1];
+}
 
 module.exports = Blockchain;
