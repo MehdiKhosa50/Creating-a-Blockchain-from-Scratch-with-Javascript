d899852613fce373283a864491df3ee4fdf8951f
Muhammad Muntazir Mehdi <MehdiKhosa50>
2024-05-20 09:56:38 +0500

    Run Multiple Servers

--- Api.js
+++ Api.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 const {v4 : uuidv4} =require('uuid');
 const nodeAddress = uuidv4().split('-').join();
 
+const port = process.argv[2];
+
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
@@ -29,6 +31,7 @@ app.get('/mine' , function(req,res){
   }
   const nonce = bitcoin.proofOfWork(previousBlockHash,currentBlockData);
   const blockHash = bitcoin.createHashBlock(previousBlockHash,currentBlockData,nonce);
+  bitcoin.createNewTransactions(100,"000000",nodeAddress);
   const newBlock = bitcoin.createNewBlock(nonce,previousBlockHash,blockHash);
   res.json({
     note: "The new Block has been Mined Successfully!",
@@ -36,6 +39,14 @@ app.get('/mine' , function(req,res){
   });
 });
 
-app.listen(3000,function(){
-    console.log("Server is Running");
+app.get('/wallet', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/wallet', function (req, res) {
+    const blockIndex = bitcoin.createNewTransactions(req.body.amount, req.body.sender, req.body.receiver);
+    res.json({ note: `The new transaction has been added to block ${blockIndex}` });
+});
+app.listen(port,function(){
+    console.log(`Server is Running in ${port}`);
 });
